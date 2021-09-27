57d9b8458bd0a6202bd892991c2169ce33b4963e
Mateus Carvalho <mateuscrvlh@alu.ufc.br>
2021-09-27 19:47:48 -0300

    2 commit da atvd1

--- atvd_1_POO.cpp
+++ atvd_1_POO.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    cout<<"Eai Sena, tudo bem? Espero que nossa jornada em POO seja divertida";
+    cout<<"Eai Sena, tudo bem? Espero que nossa jornada em POO seja divertida de verdade";
     return 0;
 }
